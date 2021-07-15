7f354f7750a0311fef8e53c2bbcf6b20cdd0d01a
Bradley Lewis <brdllws@gmail.com>
2021-07-15 13:10:03 -0400

    Added function to play a 5 round game

--- script.js
+++ script.js
@@ -20,21 +20,39 @@ function playRound(playerSelection, computerSelection) {
     let win = `You win! ${playerSelection} beats ${computerSelection}`
     let lose = `You lose! ${computerSelection} beats ${playerSelection}`
     if (playerSelection == 'rock') {
-        if (computerSelection == 'rock') return 'Draw!';
-        else if (computerSelection == 'paper') return lose;
-        else if (computerSelection == 'scissors') return win;
+        if (computerSelection == 'rock') return ['Draw!', 0];
+        else if (computerSelection == 'paper') return [lose, false];
+        else if (computerSelection == 'scissors') return [win, true];
         else throw 'Error';
     }
     if (playerSelection == 'paper') {
-        if (computerSelection == 'rock') return win;
-        else if (computerSelection == 'paper') return 'Draw!';
-        else if (computerSelection == 'scissors') return lose;
+        if (computerSelection == 'rock') return [win, true];
+        else if (computerSelection == 'paper') return ['Draw!', 0];
+        else if (computerSelection == 'scissors') return [lose, false];
         else throw 'Error';
     }
     if (playerSelection == 'scissors') {
-        if (computerSelection == 'rock') return lose;
-        else if (computerSelection == 'paper') return win;
-        else if (computerSelection == 'scissors') return 'Draw!';
+        if (computerSelection == 'rock') return [lose, false];
+        else if (computerSelection == 'paper') return [win, true];
+        else if (computerSelection == 'scissors') return ['Draw!', 0];
         else throw 'Error';
     }
+}
+
+function game() {
+    let playerPts = 0;
+    let computerPts = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerMove = prompt("Select your move", "Rock, Paper, or Scissors");
+        let result = playRound(playerMove, computerPlay());
+        let playerWin = result[1];
+        console.log(result[0]);
+        if (result[0] != 'Draw!') {
+            if (playerWin) playerPts++;
+            else computerPts++;
+        }
+    }
+    if (playerPts == computerPts) console.log("Game ends in a draw.");
+    else if (playerPts > computerPts) console.log("You win!");
+    else console.log("You lose! :(");
 }
